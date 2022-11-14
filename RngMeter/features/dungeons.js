6a5b46fd5b21856e2afd226323ae1b391c4647d4
eatplastic <117698474+eatpIastic@users.noreply.github.com>
2022-11-14 02:06:18 -0600

    minor refactoring + major dungeons improvements
    +view all dungeon rngs
    +no need to go back to rng menu to change floor
    +auto floor detection

--- RngMeter/features/dungeons.js
+++ RngMeter/features/dungeons.js
@@ -3,84 +3,108 @@ import config from "../config";
 import { dungeons_rng } from "../utils/dungeons_rng";
 import { abc, short_number } from "../utils/utils";
 import "../utils/utils"
+
 let all_floors = [19,20,21,22,23,24,25,28,29,30,31,32,33,34]
 let all_floors_name = ["F1","F2","F3","F4","F5","F6","F7","M1","M2","M3","M4","M5","M6","M7"]
-let all_floors_name_full = ["Floor I","Floor II","Floor III","Floor IV","Floor V","Floor VI","Floor VII","Floor I","Floor II","Floor III","Floor IV","Floor V","Floor VI","Floor VII"]
-register("chat", (floor, rng) => {
-    if(floor.includes(all_floors_name[config.config_floor])){
-        data.dungeon_score = 0;
-        data.dungeon_rng = rng;
-        data.save();
-    }
-}).setCriteria("You set your Catacombs (${floor}) RNG Meter to drop ${rng}!");
-register("chat", (floor) => {
-    if(floor.includes(all_floors_name[config.config_floor])){
-        data.dungeon_score = 0;
-        data.dungeon_rng = null;
-        data.save();
-    }
-}).setCriteria("You reset your selected drop for your Catacombs (${floor}) RNG Meter!");
-let is_in_floor = false;
-register("chat", (dung_type, floor) => {
-    let dungeons_mode = config.config_floor <= 6 ?"The" : "Master Mode";
-    if(dung_type.includes(`${dungeons_mode}`) && floor == all_floors_name_full[config.config_floor]){
-        is_in_floor = true;
-    }
-}).setCriteria("${}${dung_type} Catacombs - ${floor}");
-register("chat", () => is_in_floor = false).setCriteria("${*}${*} Catacombs - ${*} Stats");
-register("chat", (score, rank) => {
-    if(!config.config_display) return;
-    if(is_in_floor){
-        if(rank == "(S)"){
-            data.dungeon_score += Math.trunc(score*.7);
-            data.save();
-        }else if(rank == "(S+)"){
-            data.dungeon_score += parseInt(score);
-            data.save();
-        }
-    }
-}).setCriteria("${*}Team Score: ${score} ${rank}");
+let should_add = null;
+let current_floor = null;
+
 register("step", () => {
-    if(!config.config_display || !World.isLoaded()) return;
-    else if(Player.getContainer() !=null && Player.getContainer().getName() == "Catacombs RNG Meter") {
-        let score = Player.getContainer().getStackInSlot(all_floors[config.config_floor]);
-        if(!score || score == null || !score.getLore() || score.getLore() == null) return;
-        else if(score.getLore()[17].includes("selected. Choose one to start")) {
-            data.dungeon_score = 0;
-            data.dungeon_rng = null;
-            data.save();
-            return;
+    if(!World.isLoaded()) return;
+    try {
+        let scoreb = Scoreboard.getLines(false)
+        if(scoreb.length == 0) return;
+        if(scoreb[3].getName().includes("The Cata")) {
+            current_floor = scoreb[3].getName().substring(scoreb[3].getName().indexOf("(")+1, scoreb[3].getName().indexOf(")"))
+            if(config.config_auto_detect_floor) {config.config_floor = all_floors_name.indexOf(current_floor)}
+        } else if (scoreb[2].getName().includes("The Cata")) {
+            current_floor = scoreb[2].getName().substring(scoreb[2].getName().indexOf("(")+1, scoreb[2].getName().indexOf(")"))
+            if(config.config_auto_detect_floor) {config.config_floor = all_floors_name.indexOf(current_floor)}
+        } else {
+            current_floor = null;
         }
-        data.dungeon_score = parseInt(score.getLore()[20].split("/")[0].removeFormatting().trim().replace(",",""));
-        data.dungeon_rng = score.getLore()[17].removeFormatting();
-        data.save();
+    } catch(error) {console.log(error);}
+}).setFps(1)
+
+register("chat", (floor, rng) => {
+    console.log("set" + floor + " " + rng)
+    data.dungeons_rng[all_floors_name.indexOf(floor)] = rng;
+    data.save();
+}).setCriteria("You set your Catacombs (${floor}) RNG Meter to drop ${rng}!");
+
+register("chat", (floor) => {
+    console.log(floor + " reset")
+    data.dungeons_rng[all_floors_name.indexOf(floor)] = null;
+    data.save();
+}).setCriteria("You reset your selected drop for your Catacombs (${floor}) RNG Meter!");
+
+register("chat", () => {
+    should_add = false;
+    console.log("should_add: " + should_add)
+}).setCriteria("${*} Catacombs - ${*} Stats");
+
+register("worldLoad", () => {
+    should_add = true;
+})
+
+register("chat", (score, rank) => {
+    if(!should_add || !current_floor) return;
+    if(data.dungeon_score[all_floors_name.indexOf(current_floor)]>=dungeons_rng[i <= 6 ? "Normal_Mode" : "Master_Mode"][all_floors_name.indexOf(current_floor)][data.dungeon_rng[all_floors_name.indexOf(current_floor)]]) {
+        data.dungeon_score[all_floors_name.indexOf(current_floor)] = (data.dungeon_score[all_floors_name.indexOf(current_floor)] + Math.trunc(score*(rank=="(S)" ? .7 : rank=="(S+)" ? 1 : 0)))-dungeons_rng[i <= 6 ? "Normal_Mode" : "Master_Mode"][all_floors_name.indexOf(current_floor)][data.dungeon_rng[all_floors_name.indexOf(current_floor)]]
+    }else {
+        data.dungeon_score[all_floors_name.indexOf(current_floor)] += Math.trunc(score*(rank=="(S)" ? .7 : rank=="(S+)" ? 1 : 0))
+    }
+    data.save();
+}).setCriteria("${*}Team Score: ${score} ${rank}");
+
+register("step", () => {
+    if(!World.isLoaded()) return;
+    else if(Player.getContainer() !=null && Player.getContainer().getName() == "Catacombs RNG Meter") {
+        for(let i=0; i<all_floors.length; i++) {
+            let score = Player.getContainer().getStackInSlot(all_floors[i]);
+            if(!score || score == null || !score.getLore() || score.getLore() == null) return;
+            else if(score.getLore()[17].includes("selected. Choose one to start")) {
+                data.dungeon_score[i] = 0;
+                data.dungeon_rng[i] = null;
+                data.save();
+                return;
+            }
+            data.dungeon_score[i] = parseInt(score.getLore()[20].split("/")[0].removeFormatting().trim().replace(",",""));
+            data.dungeon_rng[i] = score.getLore()[17].removeFormatting();
+            data.save();
+        }
     }
 }).setFps(2);
+
 register("renderOverlay", () => {
     if(!config.config_display || config.config_type == 1 || !World.isLoaded()) return;
-    if(data.dungeon_score == 0 || data.dungeon_rng == null) return;
-    if (abc.isOpen()) {
-        const txt = "Click anywhere to move!"
-        Renderer.drawStringWithShadow(txt, Renderer.screen.getWidth()/2 - Renderer.getStringWidth(txt)/2, Renderer.screen.getHeight()/2)
-    }
-    if (!data.dungeon_score && abc.isOpen()) return;
-    let dungeons_type = config.config_floor <= 6 ? "Normal_Mode" : "Master_Mode";
-    let dung_rng = dungeons_rng[dungeons_type][config.config_floor][data.dungeon_rng];
-    if(dung_rng == undefined) {
-        data.dungeon_score = 0;
-        data.dungeon_rng = null;
-        data.save();
+    if(data.dungeon_score.length == 0 || data.dungeon_rng.length == 0) {
+        Renderer.drawStringWithShadow(`${PREFIX} &fOpen The Catacombs RNG Menu!`, data.x, data.y);
         return;
     }
-    let floor_colored = config.config_floor <= 6 ? `&a` : `&4`;
-    let tscore = data.dungeon_score >= dung_rng ?`&6`:`&7`;
-    let rng_progress = ((data.dungeon_score/dung_rng)*100).toFixed(2) > 100 ?"100%":`${((data.dungeon_score/dung_rng)*100).toFixed(2)}%`;
-    if(config.config_style==0) {
-        Renderer.drawStringWithShadow(`${PREFIX} ${floor_colored}${all_floors_name_full[config.config_floor]}\n&dRNG: ${dungeons_rng.Item_Names[data.dungeon_rng]}\n&dScore: ${tscore}${short_number(data.dungeon_score)}&d/&6${short_number(dung_rng)} ${tscore}(${rng_progress})`, data.x, data.y)
+
+    if (abc.isOpen()) {
+        Renderer.drawStringWithShadow("Click anywhere to move!", Renderer.screen.getWidth()/2 - Renderer.getStringWidth("Click anywhere to move!")/2, Renderer.screen.getHeight()/2)
+    }
+
+    if(config.config_display_all) {
+        if(config.config_style == 0) {
+            Renderer.drawStringWithShadow(`${PREFIX}`,data.x,data.y-10)
+        }
+        for(let i=((config.config_display_mm&&!config.config_display_normal) ? 7 : 0); i<all_floors.length-(config.config_display_mm ? 0 : 7); i++) {
+            let score = data.dungeon_score[i];
+            let rng = dungeons_rng.Item_Names[data.dungeon_rng[i]];
+            let meter_rng = dungeons_rng[i <= 6 ? "Normal_Mode" : "Master_Mode"][i][data.dungeon_rng[i]];
+            if(score == 0 || rng == null) continue;
+            let txt = config.config_style == 0 ? `${i<=6 ? "&a" : "&c"}${all_floors_name[i]}&f: &f${rng} &7${short_number(score)}&d/&6${short_number(meter_rng)} &7(${((score/meter_rng)*100).toFixed(2)}%)` : `${i<=6 ? "&a" : "&c"}${rng}: &7${short_number(score)}${i<=6 ? "&a" : "&c"}/&7${short_number(meter_rng)} &7(${((score/meter_rng)*100).toFixed(2)}%)`;
+            Renderer.drawStringWithShadow(txt, data.x, data.y + ((i-((config.config_display_mm&&!config.config_display_normal) ? 7 : 0))*10));
+        }
     } else {
-        let dung_color = dungeons_type == "Normal_Mode" ? "a" : "c";
-        let tscore = data.dungeon_score >= dung_rng ?`&b`:`&7`;
-        Renderer.drawStringWithShadow(`&${dung_color}RNG: ${dungeons_rng.Item_Names[data.dungeon_rng]}\n&${dung_color}Score: ${tscore}${short_number(data.dungeon_score)}&7/&b${short_number(dung_rng)} ${tscore}(${rng_progress})`, data.x, data.y)
+        let score = data.dungeon_score[config.config_floor];
+        let rng = dungeons_rng.Item_Names[data.dungeon_rng[config.config_floor]];
+        let meter_rng = dungeons_rng[config.config_floor <= 6 ? "Normal_Mode" : "Master_Mode"][config.config_floor][data.dungeon_rng[config.config_floor]];
+        if(score == 0 || rng == null) return;
+        let txt = config.config_style == 0 ? `${PREFIX}\n${config.config_floor<=6 ? "&a" : "&c"}${all_floors_name[config.config_floor]}&f: &f${rng}\n${config.config_floor<=6 ? "&a" : "&c"}Score&f: &7${short_number(score)}&d/&6${short_number(meter_rng)} &7(${((score/meter_rng)*100).toFixed(2)}%)` : `${config.config_floor<=6 ? "&a" : "&c"}${rng}: &7${short_number(score)}${config.config_floor<=6 ? "&a" : "&c"}/&7${short_number(meter_rng)} &7(${((score/meter_rng)*100).toFixed(2)}%)`;
+        Renderer.drawStringWithShadow(txt, data.x, data.y);
     }
 });
-register("worldLoad", () => is_in_floor = false);
